507df65fc4d32e8396a123113ecb24d282dd1386
AxVultis <axvultis@gmail.com>
2021-03-29 08:23:42 +0100

    Update Windows build

--- build.bat
+++ build.bat
@@ -3,13 +3,12 @@
 SET build_type=%~1
 SET num_thread=%~2
 SET result_folder=results\"%build_type%"-"%num_thread%"
-mkdir -p "%result_folder%"
-mkdir -p build
-DEL /S build\*
+mkdir "%result_folder%"
+mkdir build
+DEL /S /q build\*
 cd build
-cmake  .. -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE="%build_type%" -DCMAKE_EXPORT_COMPILE_COMMANDS=True
+cmake  .. -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE="%build_type%"
 msbuild concealX.sln /p:Configuration=Release /m:"%num_thread%"
 cd ..
-COPY  build\compile_commands.json "%result_folder%"
-COPY  build\src\conceal* "%result_folder%"
-COPY  build\src\walletd "%result_folder%"
+COPY  build\src\"%build_type%"\conceal*.exe "%result_folder%"
+COPY  build\src\"%build_type%"\walletd.exe "%result_folder%"
