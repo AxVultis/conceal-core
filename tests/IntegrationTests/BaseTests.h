d889534f01a92d60deb89465d9f3e0bbd46e9c4c
AxVultis <axvultis@protonmail.com>
2023-11-09 22:09:35 +0100

    Fix macOS build

--- tests/IntegrationTests/BaseTests.h
+++ tests/IntegrationTests/BaseTests.h
@@ -44,16 +44,15 @@ protected:
 
 namespace cn
 {
+  inline bool operator==(const TransactionShortInfo &a, const TransactionShortInfo &b)
+  {
+    return a.txId == b.txId;
+  }
   inline bool operator==(const BlockShortEntry &a, const BlockShortEntry &b)
   {
     return a.blockHash == b.blockHash &&
            a.hasBlock == b.hasBlock &&
            // a.block == b.block &&
            a.txsShortInfo == b.txsShortInfo;
-  }
-
-  inline bool operator==(const TransactionShortInfo &a, const TransactionShortInfo &b)
-  {
-    return a.txId == b.txId;
   }
 }
