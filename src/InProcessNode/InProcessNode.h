70be846383fdaae263e0035ffd2e75f3034390b2
AxVultis <axvultis@protonmail.com>
2023-11-11 18:14:25 +0100

    Fix warnings

--- src/InProcessNode/InProcessNode.h
+++ src/InProcessNode/InProcessNode.h
@@ -33,48 +33,48 @@ public:
   InProcessNode& operator=(const InProcessNode&) = delete;
   InProcessNode& operator=(InProcessNode&&) = delete;
 
-  virtual ~InProcessNode();
+  ~InProcessNode() override;
 
-  virtual void init(const Callback& callback) override;
-  virtual bool shutdown() override;
+  void init(const Callback& callback) override;
+  bool shutdown() override;
 
-  virtual bool addObserver(INodeObserver* observer) override;
-  virtual bool removeObserver(INodeObserver* observer) override;
+  bool addObserver(INodeObserver* observer) override;
+  bool removeObserver(INodeObserver* observer) override;
 
-  virtual size_t getPeerCount() const override;
-  virtual uint32_t getLastLocalBlockHeight() const override;
-  virtual uint32_t getLastKnownBlockHeight() const override;
-  virtual uint32_t getLocalBlockCount() const override;
-  virtual uint32_t getKnownBlockCount() const override;
-  virtual uint64_t getLastLocalBlockTimestamp() const override;
+  size_t getPeerCount() const override;
+  uint32_t getLastLocalBlockHeight() const override;
+  uint32_t getLastKnownBlockHeight() const override;
+  uint32_t getLocalBlockCount() const override;
+  uint32_t getKnownBlockCount() const override;
+  uint64_t getLastLocalBlockTimestamp() const override;
 
-  virtual void getNewBlocks(std::vector<crypto::Hash>&& knownBlockIds, std::vector<cn::block_complete_entry>& newBlocks, uint32_t& startHeight, const Callback& callback) override;
-  virtual void getTransactionOutsGlobalIndices(const crypto::Hash& transactionHash, std::vector<uint32_t>& outsGlobalIndices, const Callback& callback) override;
-  virtual void getRandomOutsByAmounts(std::vector<uint64_t>&& amounts, uint64_t outsCount,
+  void getNewBlocks(std::vector<crypto::Hash>&& knownBlockIds, std::vector<cn::block_complete_entry>& newBlocks, uint32_t& startHeight, const Callback& callback) override;
+  void getTransactionOutsGlobalIndices(const crypto::Hash& transactionHash, std::vector<uint32_t>& outsGlobalIndices, const Callback& callback) override;
+  void getRandomOutsByAmounts(std::vector<uint64_t>&& amounts, uint64_t outsCount,
       std::vector<cn::COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount>& result, const Callback& callback) override;
-  virtual void relayTransaction(const cn::Transaction& transaction, const Callback& callback) override;
-  virtual void queryBlocks(std::vector<crypto::Hash>&& knownBlockIds, uint64_t timestamp, std::vector<BlockShortEntry>& newBlocks,
+  void relayTransaction(const cn::Transaction& transaction, const Callback& callback) override;
+  void queryBlocks(std::vector<crypto::Hash>&& knownBlockIds, uint64_t timestamp, std::vector<BlockShortEntry>& newBlocks,
     uint32_t& startHeight, const Callback& callback) override;
-  virtual void getPoolSymmetricDifference(std::vector<crypto::Hash>&& knownPoolTxIds, crypto::Hash knownBlockId, bool& isBcActual,
+  void getPoolSymmetricDifference(std::vector<crypto::Hash>&& knownPoolTxIds, crypto::Hash knownBlockId, bool& isBcActual,
           std::vector<std::unique_ptr<ITransactionReader>>& newTxs, std::vector<crypto::Hash>& deletedTxIds, const Callback& callback) override;
-  virtual void getMultisignatureOutputByGlobalIndex(uint64_t amount, uint32_t gindex, MultisignatureOutput& out, const Callback& callback) override;
+  void getMultisignatureOutputByGlobalIndex(uint64_t amount, uint32_t gindex, MultisignatureOutput& out, const Callback& callback) override;
 
 
-  virtual void getBlocks(const std::vector<uint32_t>& blockHeights, std::vector<std::vector<BlockDetails>>& blocks, const Callback& callback) override;
-  virtual void getBlocks(const std::vector<crypto::Hash>& blockHashes, std::vector<BlockDetails>& blocks, const Callback& callback) override;
-  virtual void getTransaction(const crypto::Hash &transactionHash, cn::Transaction &transaction, const Callback &callback) override;
-  virtual void getBlocks(uint64_t timestampBegin, uint64_t timestampEnd, uint32_t blocksNumberLimit, std::vector<BlockDetails>& blocks, uint32_t& blocksNumberWithinTimestamps, const Callback& callback) override;
-  virtual void getTransactions(const std::vector<crypto::Hash>& transactionHashes, std::vector<TransactionDetails>& transactions, const Callback& callback) override;
-  virtual void getTransactionsByPaymentId(const crypto::Hash& paymentId, std::vector<TransactionDetails>& transactions, const Callback& callback) override;
-  virtual void getPoolTransactions(uint64_t timestampBegin, uint64_t timestampEnd, uint32_t transactionsNumberLimit, std::vector<TransactionDetails>& transactions, uint64_t& transactionsNumberWithinTimestamps, const Callback& callback) override;
-  virtual void isSynchronized(bool& syncStatus, const Callback& callback) override;
+  void getBlocks(const std::vector<uint32_t>& blockHeights, std::vector<std::vector<BlockDetails>>& blocks, const Callback& callback) override;
+  void getBlocks(const std::vector<crypto::Hash>& blockHashes, std::vector<BlockDetails>& blocks, const Callback& callback) override;
+  void getTransaction(const crypto::Hash &transactionHash, cn::Transaction &transaction, const Callback &callback) override;
+  void getBlocks(uint64_t timestampBegin, uint64_t timestampEnd, uint32_t blocksNumberLimit, std::vector<BlockDetails>& blocks, uint32_t& blocksNumberWithinTimestamps, const Callback& callback) override;
+  void getTransactions(const std::vector<crypto::Hash>& transactionHashes, std::vector<TransactionDetails>& transactions, const Callback& callback) override;
+  void getTransactionsByPaymentId(const crypto::Hash& paymentId, std::vector<TransactionDetails>& transactions, const Callback& callback) override;
+  void getPoolTransactions(uint64_t timestampBegin, uint64_t timestampEnd, uint32_t transactionsNumberLimit, std::vector<TransactionDetails>& transactions, uint64_t& transactionsNumberWithinTimestamps, const Callback& callback) override;
+  void isSynchronized(bool& syncStatus, const Callback& callback) override;
 
 private:
-  virtual void peerCountUpdated(size_t count) override;
-  virtual void lastKnownBlockHeightUpdated(uint32_t height) override;
-  virtual void blockchainSynchronized(uint32_t topHeight) override;
-  virtual void blockchainUpdated() override;
-  virtual void poolUpdated() override;
+  void peerCountUpdated(size_t count) override;
+  void lastKnownBlockHeightUpdated(uint32_t height) override;
+  void blockchainSynchronized(uint32_t topHeight) override;
+  void blockchainUpdated() override;
+  void poolUpdated() override;
 
   void getNewBlocksAsync(std::vector<crypto::Hash>& knownBlockIds, std::vector<cn::block_complete_entry>& newBlocks, uint32_t& startHeight, const Callback& callback);
   std::error_code doGetNewBlocks(std::vector<crypto::Hash>&& knownBlockIds, std::vector<cn::block_complete_entry>& newBlocks, uint32_t& startHeight);
